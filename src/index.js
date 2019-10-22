527d7c10860f75b7d9d566a2edc750f5a328a37e
Olena Gladun <helen.gladun@gmail.com>
2019-10-22 08:55:50 +0300

    Add observable object and array

--- src/index.js
+++ src/index.js
@@ -5,19 +5,31 @@ import * as serviceWorker from './serviceWorker';
 import {observable} from "mobx";
 import {observer} from "mobx-react";
 
-const counterState = observable({
-    count: 0,
+const nickName = observable({
+    firstName: 'Helen',
+    age: 30,
+
+    get nickName() {
+        console.log('Generate nickname');
+        return `${this.firstName} ${this.age}`;
+    },
+
+    increment() {
+        this.age++;
+    },
+
+    decrement() {
+        this.age--;
+    }
 });
 
-counterState.increment = function () {
-    this.count++;
-};
+const todos = observable([
+    {text: 'Learn React'},
+    {text: 'Learn MobX'},
+]);
 
-counterState.decrement = function () {
-    this.count--;
-};
-
-@observer class Counter extends Component {
+@observer
+class Counter extends Component {
     handleIncrement = () => this.props.store.increment();
 
     handleDecrement = () => this.props.store.decrement();
@@ -25,16 +37,23 @@ counterState.decrement = function () {
     render() {
         return (
             <div className="App">
-                <h1>{this.props.store.count}</h1>
+                <h1>{this.props.store.nickName}</h1>
+                <h1>{this.props.store.age}</h1>
                 <button onClick={this.handleIncrement}>+1</button>
                 <button onClick={this.handleDecrement}>-1</button>
+                <ul>
+                    {todos.map(item => (<li key={item.text}>{item.text}</li>))}
+                </ul>
             </div>
         );
     }
 }
 
-ReactDOM.render(<Counter store={counterState}/>, document.getElementById('root'));
+ReactDOM.render(<Counter store={nickName}/>, document.getElementById('root'));
 
+todos.push({
+    text: 'Learn Node.js'
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
