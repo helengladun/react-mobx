38ede63c2b27c18a7d92fbcafc21a8e011b9d655
Olena Gladun <helen.gladun@gmail.com>
2019-10-29 21:34:31 +0200

    update package.json

--- src/index.js
+++ src/index.js
@@ -2,52 +2,112 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {observable, configure, action} from "mobx";
-import {observer} from "mobx-react";
 
-configure({enforceActions: 'observed'});
+// class Store {
+//     @observable devsList = [
+//         { name: "Jack", sp: 12 },
+//         { name: "Max", sp: 10 },
+//         { name: "Leo", sp: 8 }
+//     ];
+//
+//     @computed get totalSum() {
+//         return 0;
+//     };
+//
+//     @computed get topPerformer() {
+//         return {
+//             name: "Name"
+//         }
+//     }
+//
+//     @action clearList() {
+//         this.devsList = [];
+//     }
+//
+//     @action addDeveloper(dev) {
+//         this.devsList.push(dev);
+//     }
+// }
+//
+// const store = new Store();
+//
+// const Row = ({ data: {name, sp}}) => {
+//     return (
+//         <tr>
+//             <td>{name}</td>
+//             <td>{sp}</td>
+//         </tr>
+//     )
+// };
+//
+// @observer class Table extends Component {
+//     render() {
+//         const {store} = this.props;
+//
+//         return (
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <td>Name: </td>
+//                         <td>SP: </td>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     { store.devsList.map((dev, i) => <Row key={i} data={dev} />)}
+//                 </tbody>
+//                 <tfoot>
+//                     <tr>
+//                         <td>Team SP:</td>
+//                         <td>{store.totalSum}</td>
+//                     </tr>
+//                 <tr>
+//                     <td>Top Performer: </td>
+//                     <td>{store.topPerformer ? store.topPerformer.name: ''}</td>
+//                 </tr>
+//                 </tfoot>
+//             </table>
+//         )
+//     }
+// }
+//
+// class Controls extends Component {
+//     addDeveloper = () => {
+//         const name = prompt('The name:');
+//         const sp = parseInt(prompt("The story points: "), 10);
+//         this.props.store.addDeveloper({name, sp});
+//     };
+//
+//     clearList = () => { this.props.store.clearList(); };
+//
+//     render() {
+//         return (
+//             <div className="controls">
+//                 <button onClick={this.clearList}>Clear table</button>
+//                 <button onClick={this.addDeveloper}>Add record</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Sprint Board: </h1>
+//                 <Controls store={appStore} />
+//                 <Table store={appStore} />
+//             </div>
+//         )
+//     }
+// }
 
-const nickName = observable({
-    firstName: 'Helen',
-    age: 30,
+const store = {};
 
-    get nickName() {
-        console.log('Generate nickname');
-        return `${this.firstName} ${this.age}`;
-    },
+const App = () => (
+    <div>123</div>
+);
 
-    increment() {
-        this.age++;
-    },
-
-    decrement() {
-        this.age--;
-    }
-}, {
-    increment: action('@@counter/PLUS'),
-    decrement: action('@@counter/MINUS')
-}, {
-    name: 'nickNameObservableObject'
-});
-
-@observer class Counter extends Component {
-    handleIncrement = () => this.props.store.increment();
-
-    handleDecrement = () => this.props.store.decrement();
-
-    render() {
-        return (
-            <div className="App">
-                <h1>{this.props.store.nickName}</h1>
-                <h1>{this.props.store.age}</h1>
-                <button onClick={this.handleIncrement}>+1</button>
-                <button onClick={this.handleDecrement}>-1</button>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<Counter store={nickName}/>, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
